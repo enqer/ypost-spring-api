5f8dafd88cdd403c3367ce307ae2c5aa2702d6d4
Paweł <pawel.kam4@gmail.com>
2023-08-26 19:21:04 +0200

    starting a project

---
+++ ypost/src/test/java/com/github/enqer/ypost/YpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.enqer.ypost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

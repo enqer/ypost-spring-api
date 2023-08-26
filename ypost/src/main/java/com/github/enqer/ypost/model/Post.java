b8c9d9589842642e6de9f60f2fdecde4b0562a0e
Paweł <pawel.kam4@gmail.com>
2023-08-26 20:56:48 +0200

    creating post class

---
+++ ypost/src/main/java/com/github/enqer/ypost/model/Post.java
@@ -0,0 +1,10 @@
+package com.github.enqer.ypost.model;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Long id;
+    private Long authorId;
+    private String content;
+    private LocalDateTime dateTime;
+}

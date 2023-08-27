1ce6363c5f98b5f9d0f82143275634310d424aad
Paweł <pawel.kam4@gmail.com>
2023-08-27 22:17:10 +0200

    comment class

---
+++ ypost/src/main/java/com/github/enqer/ypost/model/Comment.java
@@ -0,0 +1,11 @@
+package com.github.enqer.ypost.model;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+
+    private Long id;
+    private Long AuthorId;
+    private String content;
+    private LocalDateTime publishedAt;
+}
